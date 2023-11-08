12ffb85d8df0a38ac2b5abad82e3f0baca4ae9cf
KelvinLai2912 <kelvin.lai@live.nl>
2023-11-08 14:17:27 +0100

    update

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MealModule } from '@avans-nx-workshop/backend/features'
 
 @Module({
-  imports: [],
+  imports: [MealModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/backend/dto/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/dto.module';
+export * from './lib/meal.dto';
+export * from './lib/api-response.interceptor';
+
+
